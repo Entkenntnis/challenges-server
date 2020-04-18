46164867333f2de6d88fefe4ce8dcad0a8766394
Entkenntnis <hack@arrrg.de>
2020-04-18 19:34:54 +0200

    minimal web server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res){
+  res.send('Hello World')
+})
+
+app.listen(3000)
+console.log('Express started on port 3000')
